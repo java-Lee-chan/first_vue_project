a7f71b0c34bcf1cffa31c81bc6f420c609fccd21
java-lee-chan <381460423@qq.com>
2019-06-30 11:01:01 +0800

    需要先学vuex

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -5,7 +5,7 @@
             @before-enter="beforeEnter"
             @enter="enter"
             @after-enter="afterEnter">
-            <div class="ball" v-show="ballFlag"></div>
+            <div class="ball" v-show="ballFlag" ref="ball"></div>
         </transition>
         <!-- 商品轮播图区域 -->
         <div class="mui-card">
@@ -24,10 +24,15 @@
                         市场价: <del>￥{{ goodsinfo.market_price }}</del>&nbsp;&nbsp;
                         销售价：<span class="now-price">￥{{ goodsinfo.sell_price }}</span>
                     </p>
-                    <p>购买数量：<numbox></numbox></p>
+                    <p>购买数量：<numbox @getcount="getSelectedCount" :max="goodsinfo.stock_quantity"></numbox></p>
                     <p>
                         <mt-button type="primary" size="small">立即购买</mt-button>
                         <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
+                        <!-- 分析： 如何实现加入购物车时候，拿到 选择的数量 -->
+                        <!-- 1. 按钮属于 goodsinfo 页面，数字 属于 numbbox 组件 -->
+                        <!-- 2. 涉及到了 父子组件 的嵌套，无法直接在 goodsinfo 页面中 获取到 选中的商品数量值 -->
+                        <!-- 3. 如何解决？涉及到 子组件向父组件传值（事件调用机制） -->
+                        <!-- 4. 事件调用的本质：父向子传递方法，子调用这个方法，同时把 数据当作参数 传递给这个方法 -->
                     </p>
                 </div>
             </div>
@@ -60,7 +65,8 @@ export default {
             id: this.$route.params.id,
             lunbolist:[],   // 轮播图的数据
             goodsinfo: {},  // 获取到的商品的信息
-            ballFlag: false // 控制小球的隐藏和显示的标识符
+            ballFlag: false, // 控制小球的隐藏和显示的标识符
+            selectedCount: 1    // 保存用户选中的商品数量，默认为1
         }
     },
     created(){
@@ -109,13 +115,28 @@ export default {
             // 1. 分析导致 动画 不准确的 本质原因：小球 最终位置 已经局限在了 某一分辨率 下的 滚动条未滚动 的情况下
             // 2. 只要分辨率 和 测试的时候 不同，或者，滚动条 滚动了一定距离，问题就出现了
             // 3. 经过分析，不能把 位置的横纵坐标 直接写死，应该根据不同情况，动态计算这个坐标值 
+            // 4. 通过 徽标 的 横纵坐标 和 小球 的 横纵坐标，两者相减，就是横纵坐标要位移的距离
+            // 5. 如何 获取 徽标 和小球的位置??     domObject.getBoundingClientRect()
 
-            el.style.transform = "translate(116px, 230px)"
-            el.style.transition = "all 1s cubic-bezier(0.4, -0.3, 1, 0.68)"
+            // 获取 小球 在页面中的位置
+            const ballPosition = this.$refs.ball.getBoundingClientRect();
+            // 获取 徽标 在页面中的位置
+            const badgePosition = document.getElementById("badge").getBoundingClientRect();
+
+            const xDist = badgePosition.left - ballPosition.left;
+            const yDist = badgePosition.top - ballPosition.top;
+
+            el.style.transform = `translate(${xDist}px, ${yDist}px)`
+            el.style.transition = "all 0.5s cubic-bezier(0.4, -0.3, 1, 0.68)"
             done()
         },
         afterEnter(el){
             this.ballFlag = !this.ballFlag
+        },
+        getSelectedCount(count){
+            // 当子组件 把 选中的数量传递给父组件的时候，把选中的值保存到 data 上
+            this.selectedCount = count;
+            console.log('数量为：'+ this.selectedCount)
         }
     },
     components: {
